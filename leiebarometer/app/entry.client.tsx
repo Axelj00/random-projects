67230385ac2bb814fef8408fb3df20067f2f666b
Axelj00 <124807716+Axelj00@users.noreply.github.com>
2024-11-07 13:06:25 +0000

    fixed posthog

--- leiebarometer/app/entry.client.tsx
+++ leiebarometer/app/entry.client.tsx
@@ -1,18 +1,36 @@
-/**
- * By default, Remix will handle hydrating your app on the client for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.client
- */
+// app/entry.client.tsx
 
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import { startTransition, StrictMode, useEffect } from "react";
 import { hydrateRoot } from "react-dom/client";
+import posthog from "posthog-js";
+import { useCookies } from "react-cookie";
+
+function PosthogInit() {
+  const [cookies] = useCookies(["cookieConsent"]);
+
+  useEffect(() => {
+    if (cookies.cookieConsent === "all") {
+      posthog.init("phc_2A05w7LYR3Hc4NjFEAjRL00wqxQ6IhRHTekQq92LE75", {
+        api_host: "https://eu.i.posthog.com",
+        autocapture: true,
+        capture_pageview: true,
+        session_recording: {
+          // Optional session recording configurations
+        },
+      });
+    }
+  }, [cookies.cookieConsent]);
+
+  return null;
+}
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
       <RemixBrowser />
+      <PosthogInit />
     </StrictMode>
   );
 });
